473625fb119363034225b1251944b30db8bea346
Astha86 <asthasahani08@gmail.com>
2026-01-29 00:09:55 +0530

    setup user service with TypeScript and MongoDB connection

---
+++ backend/user-service/src/config/db.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const connectDb = async() => {
+    console.log("Connecting to MongoDB...");
+    const url = process.env.MONGO_URI;
+
+    if(!url){
+        throw new Error("MONGO_URI is not defined in environment variables");
+    }
+
+    try{
+        await mongoose.connect(url, {
+            dbName: "user-service",
+        });
+        console.log("Connected to MongoDB successfully");
+        
+    } catch(error){
+        console.error("Failed to connect to MongoDB", error);
+        process.exit(1);
+    }
+}
+
+export default connectDb;
